0557ae304656900dd9cd9e1fec938e9a095f8983
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-05 16:27:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,24 @@
 
 # Code your methods below
+def lower_body
+  puts "Plant legs far apart,bend knees slightly and keep posture loose"
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end 
+
+def skip-step
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Left,Right,Left and Left"
+  skip step right 
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right,Left,Right and Right"
+end 
+
+def upper_body
+  bob the reins 
+  puts "Hold your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This mov"
